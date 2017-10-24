00154735e3b78a7c53d006adc956e9ce53bc50ba
Andres Scarpetta <andresscarpetta@gmail.com>
2017-10-24 11:59:20 -0500

    Added key function to RemoteConfig module

--- lib/remote_config.rb
+++ lib/remote_config.rb
@@ -3,5 +3,23 @@ require "aws-sdk-lambda"
 require "json"
 
 module RemoteConfig
-  # your code goes here
+  CLIENT = Aws::Lambda::Client.new(region: "us-west-2")
+
+  def get_config_keys(key)
+    req_payload = {key: key}
+    env = ENV["ENV"]
+    if env
+      req_payload['env'] = env
+    end
+    payload = JSON.generate(req_payload)
+
+    response = CLIENT.invoke({
+      function_name: "remote-config",
+      invocation_type: "RequestResponse",
+      payload: payload
+    })
+
+    res_payload = JSON.parse(response.payload.string)
+    return res_payload["value"]
+  end
 end
